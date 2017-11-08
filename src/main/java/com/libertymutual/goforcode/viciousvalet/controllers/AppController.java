e4164dc302c9450661ce1d548841744d4ad29845
Jon Graca <jon.graca@libertymutual.com>
2017-11-08 15:00:56 -0500

    Updating with conditional statement

--- src/main/java/com/libertymutual/goforcode/viciousvalet/controllers/AppController.java
+++ src/main/java/com/libertymutual/goforcode/viciousvalet/controllers/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.libertymutual.goforcode.viciousvalet.models.Car;
 import com.libertymutual.goforcode.viciousvalet.models.Lot;
 
 @Controller
@@ -18,6 +19,20 @@ public class AppController {
 
 	@RequestMapping("/")
 	public ModelAndView showApp(String license, String state, String color, String make, String model) {
+		// if license is not null and the length of license is greater than 0
+		// create a new instance of the car class from the values submitted
+		
+		if (license != null && !license.isEmpty() 
+				&& state != null && !state.isEmpty()
+				&& color != null && !color.isEmpty()
+				&& make != null && !make.isEmpty()
+				&& model != null && !model.isEmpty()){
+			
+			Car newCar = new Car (license, state, color, make, model);
+		} else {
+			throw new EmptyStackException();
+		}
+		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("app");
 
--- src/main/java/com/libertymutual/goforcode/viciousvalet/models/Car.java
+++ src/main/java/com/libertymutual/goforcode/viciousvalet/models/Car.java
@@ -2,20 +2,38 @@ package com.libertymutual.goforcode.viciousvalet.models;
 
 public class Car {
 
-	// Methods: create, locate, park
-
 	private String license;
 	private String state;
 	private String color;
 	private String make;
 	private String model;
-	
+
 	public Car(String license, String state, String color, String make, String model) {
 		this.license = license;
 		this.state = state;
 		this.color = color;
 		this.make = make;
 		this.model = model;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
 	}
 
 }
